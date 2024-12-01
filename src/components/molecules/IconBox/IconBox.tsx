5fc7b395dbbb049179a99f2bb9309a293f0b8461
Shekoofeh Dezhahanj <sh.dezhahanj@gmail.com>
2024-12-01 17:54:16 +0330

    fix: format

--- src/components/molecules/IconBox/IconBox.tsx
+++ src/components/molecules/IconBox/IconBox.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const IconBox: FC<IProps> = ({ icon, title }) => {
 	return (
 		<div className="flex flex-col py-6 px-4 w-full border border-regular bg-background-component rounded-xl gap-3 justify-center text-center">
-			<Icon name={icon} size='3em' className='mx-auto'/>
+			<Icon name={icon} size="3em" className="mx-auto" />
 			<Typography.Text>{title}</Typography.Text>
 		</div>
 	);
