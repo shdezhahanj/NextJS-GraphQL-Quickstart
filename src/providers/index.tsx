29725aafa331427f4c4bce665ac573ba976d1aa6
Shekoofeh Dezhahanj <sh.dezhahanj@gmail.com>
2025-02-09 13:16:49 +0330

    fix: format

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,9 +2,5 @@ import { ReduxProvider } from '@/providers/ReduxProvider';
 import type { PropsWithChildren } from 'react';
 
 export const Providers = ({ children }: PropsWithChildren) => {
-	return (
-		<ReduxProvider>
-			{children}
-		</ReduxProvider>
-	);
+	return <ReduxProvider>{children}</ReduxProvider>;
 };
